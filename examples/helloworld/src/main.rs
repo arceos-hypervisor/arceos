c91629aa49144b1270252e0e310c65da9e41e6d3
hky1999 <976929993@qq.com>
2024-09-07 23:57:48 +0800

    Add a sleep loop in helloworld example for test

--- examples/helloworld/src/main.rs
+++ examples/helloworld/src/main.rs
@@ -1,10 +1,25 @@
 #![cfg_attr(feature = "axstd", no_std)]
 #![cfg_attr(feature = "axstd", no_main)]
 
+#[macro_use]
 #[cfg(feature = "axstd")]
-use axstd::println;
+extern crate axstd as std;
+
+use std::thread;
+use std::time::{Duration, Instant};
 
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
     println!("Hello, world!");
+
+    let mut sec = 1;
+
+    loop {
+        println!("ArceOS sleep {} seconds ...", sec);
+        let now = Instant::now();
+        thread::sleep(Duration::from_secs(sec as _));
+        let elapsed = now.elapsed();
+        sec += 1;
+        println!("ArceOS actual sleep {:?} seconds.", elapsed);
+    }
 }
