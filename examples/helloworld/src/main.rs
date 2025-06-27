601f24882a8dc72e1d483a5284b90cba8f0443c0
luodeb <luodeb@outlook.com>
2025-06-27 14:05:26 +0800

    feat: 添加对MMIO设备的支持并更新示例代码

--- examples/helloworld/src/main.rs
+++ examples/helloworld/src/main.rs
@@ -7,4 +7,14 @@ use axstd::println;
 #[cfg_attr(feature = "axstd", unsafe(no_mangle))]
 fn main() {
     println!("Hello, world!");
+    let data_addr = 0xfd00_0000u32 as u32;
+    // 直接读取该地址的值
+    let data: u32 = unsafe { core::ptr::read_volatile(data_addr as *const u32) };
+    println!("Data at address 0xfd00_0000: {:#x}", data);
+    // 直接写入一个新值
+    let new_value: u32 = 0x1234_5678;
+    unsafe {
+        core::ptr::write_volatile(data_addr as *mut u32, new_value);
+    }
+    println!("New value written to address 0xfd00_0000: {:#x}", new_value);
 }
