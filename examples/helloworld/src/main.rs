208f51862ab5735c615d6339a8ef8d47ae100847
luodeb <luodeb@outlook.com>
2025-06-28 01:11:07 +0800

    Implement code changes to enhance functionality and improve performance

--- examples/helloworld/src/main.rs
+++ examples/helloworld/src/main.rs
@@ -7,14 +7,15 @@ use axstd::println;
 #[cfg_attr(feature = "axstd", unsafe(no_mangle))]
 fn main() {
     println!("Hello, world!");
-    let data_addr = 0xfd00_0000u32 as u32;
+    let data_addr = 0xfd00_0008u32 as u32;
     // 直接读取该地址的值
     let data: u32 = unsafe { core::ptr::read_volatile(data_addr as *const u32) };
-    println!("Data at address 0xfd00_0000: {:#x}", data);
+    println!("Data read from address 0xfd00_0000: {:#x}", data);
     // 直接写入一个新值
-    let new_value: u32 = 0x1234_5678;
+    let new_value: u32 = 0x1232_5678;
+    println!("New value written to address 0xfd00_0000: {:#x}", new_value);
     unsafe {
         core::ptr::write_volatile(data_addr as *mut u32, new_value);
     }
-    println!("New value written to address 0xfd00_0000: {:#x}", new_value);
+    println!("OK");
 }
