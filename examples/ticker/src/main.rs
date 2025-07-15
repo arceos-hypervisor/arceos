a61a28dddf4b19b4c1e187bf9077881d9fded5a8
aarkegz <aarkegz@gmail.com>
2025-07-15 21:17:31 +0800

    fix unit-test error

--- examples/ticker/src/main.rs
+++ examples/ticker/src/main.rs
@@ -1,26 +1,26 @@
 #![cfg_attr(feature = "axstd", no_std)]
 #![cfg_attr(feature = "axstd", no_main)]
 
+use core::time::Duration;
+
+#[cfg(feature = "axstd")]
+use axstd::os::arceos::api::time::ax_monotonic_time;
+#[cfg(feature = "axstd")]
+use axstd::os::arceos::modules;
 #[cfg(feature = "axstd")]
 use axstd::println;
 
-
-use core::time::Duration;
-
-use axstd::os::arceos::api;
-use axstd::os::arceos::modules;
-use axstd::println;
-
-use api::time::ax_monotonic_time;
-
 #[cfg_attr(feature = "axstd", unsafe(no_mangle))]
 fn main() {
-    println!("Ticking!");
+    #[cfg(feature = "axstd")]
+    {
+        println!("Ticking!");
 
-    println!("Current monotonic time: {:?}", ax_monotonic_time());
-
-    loop {
-        modules::axtask::sleep(Duration::from_millis(500));
         println!("Current monotonic time: {:?}", ax_monotonic_time());
+
+        loop {
+            modules::axtask::sleep(Duration::from_millis(500));
+            println!("Current monotonic time: {:?}", ax_monotonic_time());
+        }
     }
 }
