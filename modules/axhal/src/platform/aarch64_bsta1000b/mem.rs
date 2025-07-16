d61cb890b48f386f1332ca152db56163e124e213
buhenxihuan <1127751018@qq.com>
2025-07-16 13:43:24 +0800

    bugfix for a1000-hv

--- modules/axhal/src/platform/aarch64_bsta1000b/mem.rs
+++ modules/axhal/src/platform/aarch64_bsta1000b/mem.rs
@@ -4,8 +4,8 @@ use page_table_entry::{GenericPTE, MappingFlags, aarch64::A64PTE};
 /// Returns (a1000b only) memory regions.
 pub(crate) fn default_a1000b_regions() -> impl Iterator<Item = MemRegion> {
     [MemRegion {
-        paddr: pa!(0x1_b000_0000),
-        size: 0x40000000,
+        paddr: pa!(0x8000_0000),
+        size: 0x70000000,
         flags: MemRegionFlags::RESERVED | MemRegionFlags::READ | MemRegionFlags::WRITE,
         name: "reserved memory",
     }]
