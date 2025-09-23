8f93636b5b0e65c4bc3c6eb422da384d88b0bb72
周睿 <zrufo747@outlook.com>
2025-09-23 10:00:23 +0800

    feat: add iomap api

--- modules/axmm/src/lib.rs
+++ modules/axmm/src/lib.rs
@@ -112,3 +112,19 @@ pub fn init_memory_management() {
 pub fn init_memory_management_secondary() {
     unsafe { axhal::asm::write_kernel_page_table(kernel_page_table_root()) };
 }
+
+/// Maps a physical memory region to virtual address space for device access.
+pub fn iomap(addr: PhysAddr, size: usize) -> AxResult<VirtAddr> {
+    let virt = phys_to_virt(addr);
+
+    let virt_aligned = virt.align_down_4k();
+    let addr_aligned = addr.align_down_4k();
+    let size_aligned = (addr + size).align_up_4k() - addr_aligned;
+
+    let flags = MappingFlags::DEVICE | MappingFlags::READ | MappingFlags::WRITE;
+    let mut tb = kernel_aspace().lock();
+    tb.map_linear(virt_aligned, addr_aligned, size_aligned, flags)?;
+    // flush TLB
+    tb.protect(virt_aligned, size_aligned, flags)?;
+    Ok(virt)
+}
